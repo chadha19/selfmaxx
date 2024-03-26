98be82dea94ddbbfae97276ca1edb12194b2a61c
Tanav Palisetti <56857569+TanavP2112@users.noreply.github.com>
2024-03-26 14:58:13 -0500

    hi

--- Pages/Home.tsx
+++ Pages/Home.tsx
@@ -97,6 +97,7 @@ const Home = () => {
           value={pass}
           secureTextEntry
         />
+
         {errors.pass && <Text style={styles.error}>{errors.pass}</Text>}
         <TouchableOpacity
           style={[styles.buttonStyle, {opacity: isFormValid ? 1 : 0.5}]}
